50f279437c87fd6dcd7fbace4ab3e5f9d4df3a8f
gabriel_severo <gabrielssevero@gmail.com>
2022-10-25 10:27:36 -0300

    Criando websites com Winter CMS - Parte 12 - Criando Opções para o Componente

--- plugins/zmark/content/components/Categorias.php
+++ plugins/zmark/content/components/Categorias.php
@@ -15,8 +15,64 @@ class Categorias extends ComponentBase
     ];
   }
 
+  public function defineProperties() {
+
+    return [
+      'results' => [
+        'title' => 'Número de Categorias',
+        'description' => 'Quantas categorias você quer exibir?',
+        'default' => 0,
+        'validationPattern' => '^[0-9]+$',
+        'validationMessage' => 'Apenas Números São Permitidos'
+      ],
+
+      'sortOrder' => [
+        'title' => 'Ordem das Categorias',
+        'description' => 'Ordem dos resultados das Categorias',
+        'type' => 'dropdown',
+        'default' => 'title asc'
+      ]
+
+    ];
+
+  }
+
+  public function getSortOrderOptions() {
+
+    return [
+      'title asc' => 'Título (Ascendente)',
+      'title desc' => 'Título (Descendente)',
+      'id asc' => 'Cadastro (Ascendente)',
+      'id desc' => 'Cadastro (Descendente)',
+    ];
+
+  }
+
   protected function loadCategorias() {
-    return Categoria::all();
+
+    $query = Categoria::all();
+
+
+    $sort = [$this->property('sortOrder')];
+
+    $parts = explode(' ', $sort[0]);
+
+    if($parts[1] == 'asc') {
+      $query = $query->sortBy($parts[0]);
+    } 
+    else {
+      $query = $query->sortByDesc($parts[0]);
+    }
+  
+
+    if($this->property('results') > 0) {
+
+      $query = $query->take($this->property('results'));
+
+    }
+
+    return $query;
+
   }
   
   public function onRun() {
